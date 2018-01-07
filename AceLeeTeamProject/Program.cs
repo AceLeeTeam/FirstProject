991970e3d0b0f06187f88e1f70ebc785c1a64195
Ace.Lee <daship1@gmail.com>
2018-01-07 14:47:45 +0800

    Add project files.

---
+++ AceLeeTeamProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceLeeTeamProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a hello world");
+        }
+    }
+}
